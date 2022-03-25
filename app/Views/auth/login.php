c04ee1681b9f1ad7c72323c38d0fed2681dc3f41
Micko Widi <widimicko85@gmail.com>
2022-03-25 12:52:33 +0700

    create login page

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -2,20 +2,31 @@
 
 <?= $this->section('body') ?>
 
-<div class="container my-5">
-  <h1>Login</h1>
-  <?php if($message = session()->getFlashData('failed')) : ?>
-    <div class="alert alert-danger" role="alert">
-        <?= $message ?>
-    </div>
-  <?php endif ?>
+<div class="login">
+  <main class="form-signin text-center">
+    <form action="<?= base_url('/login') ?>" method="post">
+      <img class="mb-4" src="<?= base_url('image/logo_tniad.png') ?>" alt="" width="72" height="100">
+      <h1 class="h3 mb-3 fw-normal">Sistem Absensi Koramil 0827/18 Kangean</h1>
+
+      <?php if($message = session()->getFlashData('failed')) : ?>
+        <div class="alert alert-danger" role="alert">
+            <?= $message ?>
+        </div>
+      <?php endif ?>
   
-  <form action="<?= base_url('/login') ?>" method="post">
-    <?= csrf_field() ?>
-    <input type="email" name="email" class="form-control">
-    <input type="password" name="password" class="form-control">
-    <button>Login</button>
-  </form>
+      <div class="form-floating mb-3">
+        <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <button class="w-100 btn btn-lg btn-success" type="submit">Masuk</button>
+      <p class="mt-5 mb-3 text-muted">copyright &copy; <?= date('Y') ?></p>
+    </form>
+  </main>
 </div>
 
 <?= $this->endSection() ?>
